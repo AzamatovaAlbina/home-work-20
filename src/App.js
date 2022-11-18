9f9f06153c41fe494d959d3db9fe3d2d171c77e6
Albina <azamatovaalbina7@gmail.com>
2022-11-18 20:29:49 +0600

    add hw20

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
+import useCounter from './helpers/hooks/useCounter';
 
 function App() {
+const [count , increment, decrement, reset]= useCounter(0, 7, 0, 50)
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>{count}</h1>
+      <button onClick={decrement}>-</button>
+      <button onClick={increment}>+</button>
+      <button onClick={reset}>reset</button>
     </div>
   );
 }
